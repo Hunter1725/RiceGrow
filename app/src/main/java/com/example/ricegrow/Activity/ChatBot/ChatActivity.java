a7bc755ec2d1d3b5a828087c2f12d062a0210819
Hunter <98210343+Hunter1725@users.noreply.github.com>
2023-10-14 09:46:40 +0700

    Update OpenAI Key

--- app/src/main/java/com/example/ricegrow/Activity/ChatBot/ChatActivity.java
+++ app/src/main/java/com/example/ricegrow/Activity/ChatBot/ChatActivity.java
@@ -198,7 +198,7 @@ public class ChatActivity extends AppCompatActivity implements NetworkUtils.OnCo
         RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/chat/completions")
-                .header("Authorization","Bearer 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")
+                .header("Authorization","Bearer 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")
                 .post(body)
                 .build();
 
